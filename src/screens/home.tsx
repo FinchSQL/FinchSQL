95b3150c788b79455b48b9f72e57c9f8f5305a70
Yumin Gui <guiyumin@protonmail.com>
2025-10-23 00:58:29 -0700

    feat: enhance ConnectionForm layout and add connection handling logic

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -2,10 +2,8 @@ import { ConnectionForm } from "../components/connection/ConnectionForm";
 
 export function Home() {
   return (
-    <div className="flex flex-1 bg-gray-200 h-screen overflow-y-auto">
-      <div className="flex flex-col w-full gap-6 p-4 sm:p-10">
-        <ConnectionForm />
-      </div>
+    <div className="flex flex-1 bg-gray-200 h-screen overflow-hidden items-center justify-center">
+      <ConnectionForm />
     </div>
   );
 }
